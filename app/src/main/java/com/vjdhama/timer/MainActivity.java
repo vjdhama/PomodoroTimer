8384fba1c6922aab088265712932f76bab5c94f2
vjdhama <vjdhama26@gmail.com>
2015-12-09 13:10:41 +0530

    Added configuration changes handling

--- app/src/main/java/com/vjdhama/timer/MainActivity.java
+++ app/src/main/java/com/vjdhama/timer/MainActivity.java
@@ -16,61 +16,85 @@ public class MainActivity extends AppCompatActivity {
 
     TextView txtClicks;
     Timer timer;
+    long startNewTime;
+    Button startButton;
+    boolean isTimerOn = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Button startButton = (Button) findViewById(R.id.timerButton);
+        startButton = (Button) findViewById(R.id.timerButton);
         txtClicks = (TextView) findViewById(R.id.time_text_view);
         timer = new Timer();
 
-        startButton.setOnClickListener(new MyOnClickListener(startButton));
-
+        startButton.setOnClickListener(new MyOnClickListener());
     }
 
-    class MyTimerTask extends TimerTask{
-        long startTime = new Date().getTime();
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putLong("startTime", startNewTime);
+        Log.d("Timer", "new " + startNewTime);
+        outState.putBoolean("Times", isTimerOn);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        startNewTime = savedInstanceState.getLong("startTime");
+        isTimerOn = savedInstanceState.getBoolean("Times", false);
+
+        if (isTimerOn) {
+            startTimer();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        timer.cancel();
+    }
+
+    private void startTimer() {
+        timer.schedule(new MyTimerTask(startNewTime), 0, 1000);
+    }
+
+    class MyTimerTask extends TimerTask {
+
+        long startTime;
+
+        public MyTimerTask(long startNewTime) {
+            this.startTime = startNewTime;
+        }
 
         @Override
         public void run() {
             long elapsedSecs = (new Date().getTime() - startTime) / 1000;
             final long seconds = elapsedSecs % 60;
             final long minutes = elapsedSecs / 60;
-            runOnUiThread(new Runnable() {
 
+            runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
                     txtClicks.setText(String.format("%02d:%02d", minutes, seconds));
-                    if (seconds == 10) {
+                    if (minutes == 1) {
                         Toast.makeText(MainActivity.this, "Finished", Toast.LENGTH_SHORT).show();
                         timer.cancel();
+                        isTimerOn = false;
                     }
                 }
             });
-
         }
     }
 
     private class MyOnClickListener implements View.OnClickListener {
-        private final Button startButton;
-
-        public MyOnClickListener(Button startButton) {
-            this.startButton = startButton;
-        }
-
         @Override
         public void onClick(View v) {
-            Log.d("Timer", startButton.getText().toString());
-            if (startButton.getText().toString().trim().equals("Start")){
-                if (timer == null){
-                    timer = new Timer();
-                }
-                timer.schedule(new MyTimerTask(), 0, 1000);
-                startButton.setText("Stop");
-            } else {
-                startButton.setText("Start");
-                timer.cancel();
-            }
+            if (isTimerOn) return;
+            isTimerOn = true;
+            startNewTime = new Date().getTime();
+            startTimer();
         }
     }
 }
